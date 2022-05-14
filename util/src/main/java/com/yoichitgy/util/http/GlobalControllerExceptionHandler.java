b93b91756d97c6bba6023c5d20344e2671857872
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-15 00:10:00 +0900

    Handle BadRequestException for 400 error in the API doc.

--- util/src/main/java/com/yoichitgy/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/com/yoichitgy/util/http/GlobalControllerExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.yoichitgy.util.http;
 
+import com.yoichitgy.api.exceptions.BadRequestException;
+import com.yoichitgy.api.exceptions.InvalidInputException;
+import com.yoichitgy.api.exceptions.NotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -9,12 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.yoichitgy.api.exceptions.InvalidInputException;
-import com.yoichitgy.api.exceptions.NotFoundException;
-
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public @ResponseBody HttpErrorInfo handleBadRequestException(
+        ServerHttpRequest request, BadRequestException ex
+    ) {
+        return createHttpErrorInfo(HttpStatus.BAD_REQUEST, request, ex);
+    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
