78c615b7a0fe09f3d325bdc0db81abd6594c2fc9
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-15 17:52:45 +0900

    Add recommendation entity and repository.

---
+++ microservices/recommendation-service/src/main/java/com/yoichitgy/microservices/core/recommendation/persistence/RecommendationEntity.java
@@ -0,0 +1,34 @@
+package com.yoichitgy.microservices.core.recommendation.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "recommendations")
+@CompoundIndex(name = "prod-rec-id", unique = true, def = "{'productId': 1, 'recommendationId': 1}")
+@Data
+@NoArgsConstructor
+public class RecommendationEntity {
+    @Id
+    private String id;
+    @Version
+    private Integer version;
+
+    private int productId;
+    private int recommendationId;
+    private String author;
+    private int rating;
+    private String content;
+
+    public RecommendationEntity(int productId, int recommendationId, String author, int rating, String content) {
+        this.productId = productId;
+        this.recommendationId = recommendationId;
+        this.author = author;
+        this.rating = rating;
+        this.content = content;
+    }
+}
---
+++ microservices/recommendation-service/src/main/java/com/yoichitgy/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,9 @@
+package com.yoichitgy.microservices.core.recommendation.persistence;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+    List<RecommendationEntity> findByProductId(int productId);
+}
