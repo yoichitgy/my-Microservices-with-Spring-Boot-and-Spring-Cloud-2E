430dafdda55b118fd3b0f2bd35091ea8f700ce5a
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-15 17:21:51 +0900

    Implement product entity and repository.

---
+++ microservices/product-service/src/main/java/com/yoichitgy/microservices/core/product/persistence/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.yoichitgy.microservices.core.product.persistence;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProductRepository extends PagingAndSortingRepository<ProductEntity, String> {
+    Optional<ProductEntity> findByProductId(int productId);
+}
