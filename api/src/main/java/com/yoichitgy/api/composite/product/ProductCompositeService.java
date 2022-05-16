c88cc14f98879286b2947308eaede3dca64eb553
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-16 15:53:49 +0900

    Fix wrong import of RequestBody in ProductCompositeService.

--- api/src/main/java/com/yoichitgy/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/yoichitgy/api/composite/product/ProductCompositeService.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "ProductComposite", description = "REST API for composite product information.")
