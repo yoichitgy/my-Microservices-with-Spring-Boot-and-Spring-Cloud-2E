cc97b3c2e005286ffb8d2b046a55a5fb1905a0c2
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-17 17:40:58 +0900

    Make ReviewService and its mysql access reactive (non-blocking).

--- api/src/main/java/com/yoichitgy/api/core/product/ProductService.java
+++ api/src/main/java/com/yoichitgy/api/core/product/ProductService.java
@@ -1,31 +1,13 @@
 package com.yoichitgy.api.core.product;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import reactor.core.publisher.Mono;
 
 public interface ProductService {
-    /**
-     * Sample usage, see below.
-     *
-     * curl -X POST $HOST:$PORT/product \
-     *   -H "Content-Type: application/json" --data \
-     *   '{"productId":123,"name":"product 123","weight":123}'
-     *
-     * @param body A JSON representation of the new product
-     * @return A JSON representation of the newly created product
-     */
-    @PostMapping(
-        value    = "/product",
-        consumes = MediaType.APPLICATION_JSON_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    Mono<Product> createProduct(@RequestBody Product body);
+    Mono<Product> createProduct(Product body);
 
     /**
      * Sample usage: "curl $HOST:$PORT/product/1".
@@ -36,11 +18,5 @@ public interface ProductService {
     @GetMapping(value = "/product/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
     Mono<Product> getProduct(@PathVariable int productId);
 
-    /**
-     * Sample usage: "curl -X DELETE $HOST:$PORT/product/1".
-     *
-     * @param productId Id of the product
-     */
-    @DeleteMapping(value = "/product/{productId}")
-    Mono<Void> deleteProduct(@PathVariable int productId);
+    Mono<Void> deleteProduct(int productId);
 }
--- microservices/review-service/src/main/java/com/yoichitgy/microservices/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/com/yoichitgy/microservices/core/review/services/ReviewServiceImpl.java
@@ -1,6 +1,9 @@
 package com.yoichitgy.microservices.core.review.services;
 
+import static java.util.logging.Level.FINE;
+
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import com.yoichitgy.api.core.review.Review;
 import com.yoichitgy.api.core.review.ReviewService;
@@ -11,26 +14,47 @@ import com.yoichitgy.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
 @RestController
 public class ReviewServiceImpl implements ReviewService {
     private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
 
+    private final Scheduler jdbcScheduler;
     private final ReviewRepository repository;
     private final ReviewMapper mapper;
     private final ServiceUtil serviceUtil;
 
     @Autowired
-    public ReviewServiceImpl(ReviewRepository repository, ReviewMapper mapper, ServiceUtil serviceUtil) {
+    public ReviewServiceImpl(
+        @Qualifier("jdbcScheduler") Scheduler jdbcScheduler,
+        ReviewRepository repository,
+        ReviewMapper mapper,
+        ServiceUtil serviceUtil
+    ) {
+        this.jdbcScheduler = jdbcScheduler;
         this.repository = repository;
         this.mapper = mapper;
         this.serviceUtil = serviceUtil;
     }
 
     @Override
-    public Review createReview(Review body) {
+    public Mono<Review> createReview(Review body) {
+        int productId = body.getProductId();
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+        return Mono.fromCallable(() -> _createReview(body))
+            .subscribeOn(jdbcScheduler);
+    }
+
+    private Review _createReview(Review body) {
         try {
             var entity = mapper.apiToEntity(body);
             var newEntity = repository.save(entity);
@@ -48,22 +72,39 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public List<Review> getReviews(int productId) {
+    public Flux<Review> getReviews(int productId) {
         if (productId < 1) {
             throw new InvalidInputException("Invalid productId: " + productId);
         }
-        
+        LOG.info("Will get review for product with id={}", productId);
+       
+        return Mono.fromCallable(() -> _getReviews(productId))
+            .flatMapMany(Flux::fromIterable)
+            .log(LOG.getName(), FINE)
+            .subscribeOn(jdbcScheduler);
+    }
+
+    private List<Review> _getReviews(int productId) {
         var entities = repository.findByProductId(productId);
         var response = mapper.entityListToApiList(entities);
         var address = serviceUtil.getServiceAddress();
         response.forEach(e -> e.setServiceAddress(address));
 
-        LOG.debug("getReviews: response size: {}", response.size());
+        LOG.debug("Response size: {}", response.size());
         return response;
     }
 
     @Override
-    public void deleteReviews(int productId) {
+    public Mono<Void>deleteReviews(int productId) {
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+        return Mono.fromRunnable(() -> _deleteReviews(productId))
+            .subscribeOn(jdbcScheduler)
+            .then();
+    }
+
+    private void _deleteReviews(int productId) {
         LOG.debug("deleteReviews: tries to delete reviews for the product with productId: {}", productId);
         repository.deleteAll(repository.findByProductId(productId));
     }
