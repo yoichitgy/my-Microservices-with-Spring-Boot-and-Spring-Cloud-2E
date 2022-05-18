d1d688cfc9a224eb4f728c6e993e3ad5b6f36ae6
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-18 19:38:22 +0900

    Replace Event implementation with lombok.

--- api/src/main/java/com/yoichitgy/api/event/Event.java
+++ api/src/main/java/com/yoichitgy/api/event/Event.java
@@ -5,6 +5,13 @@ import java.time.ZonedDateTime;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Event<K, T> {
     public enum Type {
         CREATE,
@@ -16,30 +23,8 @@ public class Event<K, T> {
     private final T data;
     private final ZonedDateTime eventCreatedAt;
 
-    public Event() {
-        this.eventType = null;
-        this.key = null;
-        this.data = null;
-        this.eventCreatedAt = null;
-    }
-
     public Event(Type eventType, K key, T data) {
-        this.eventType = eventType;
-        this.key = key;
-        this.data = data;
-        this.eventCreatedAt = ZonedDateTime.now();
-    }
-
-    public Type getEventType() {
-        return eventType;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public T getData() {
-        return data;
+        this(eventType, key, data, ZonedDateTime.now());
     }
 
     @JsonSerialize(using = ZonedDateTimeSerializer.class)
