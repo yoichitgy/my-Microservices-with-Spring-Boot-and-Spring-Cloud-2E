078b5e7e7540d6e356b017c9a3a3a1098252c338
Yoichi Tagaya <yoichi@el-eleven.com>
2022-05-24 13:00:01 +0900

    Let the gateway to use the authorization server.

--- spring-cloud/gateway/src/test/java/com/yoichitgy/springcloud/gateway/GatewayApplicationTests.java
+++ spring-cloud/gateway/src/test/java/com/yoichitgy/springcloud/gateway/GatewayApplicationTests.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {"eureka.client.enabled=false"})
+@SpringBootTest(
+	webEnvironment = WebEnvironment.RANDOM_PORT,
+	properties = {
+		"spring.security.oauth2.resourceserver.jwt.jwk-set-uri=some-url",
+		"eureka.client.enabled=false"
+	}
+)
 class GatewayApplicationTests {
 
 	@Test
